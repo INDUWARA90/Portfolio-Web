d7d85e71119b75058b18eb56e091d8e901634bc8
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-06-28 07:14:30 +0530

    Add Navigation-bar

---
+++ script/main.js
@@ -0,0 +1,13 @@
+const hamberger=document.querySelector(".Hamberger");
+const NavM=document.querySelector(".Navigation");
+
+hamberger.addEventListener("click",()=>{
+    hamberger.classList.toggle("active");
+    NavM.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+    hamberger.classList.remove("active");
+    NavM.classList.remove("active");
+}))
+  
