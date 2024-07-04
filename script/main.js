b62e828404d58e8885c874a67ff69b1a4a10e281
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-07-04 09:27:19 +0530

    Add Contact Me Form

--- script/main.js
+++ script/main.js
@@ -11,6 +11,7 @@ document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=
     hamberger.classList.remove("active");
     NavM.classList.remove("active");
 }));
-//=============================================================================
 
-//=========================== Scroll Event================================
+
+
+//=============================================================================
